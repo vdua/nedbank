311158887d286554c53cb8d50ae8ac03c155da73
Astha Bhargava <asthabhargava001@gmail.com>
2022-11-03 15:07:35 +0530

    updated carousel style (#31)
    * feat: related posts carousel style for mobile and desktop view

--- blocks/related-posts/related-posts.js
+++ blocks/related-posts/related-posts.js
@@ -2,35 +2,86 @@ import {
   loadBlock, buildBlock, decorateBlock, readBlockConfig,
 } from '../../scripts/scripts.js';
 
-function buildCarouselCards() {
-  const carouselCards = [];
-  for (let i = 0; i < 4; i = i + 1) {
-    const carouselCard = [];
-    const pic = document.createElement('picture');
-    pic.innerHTML = `
-      <picture>
-        <source media="(min-width: 400px)" type="image/webp" srcset="/media_1b4e25ce45be4e1dafab5a032e1fb1fbf97e7270a.png?width=2000&amp;format=webply&amp;optimize=medium">
-        <source type="image/webp" srcset="/media_1b4e25ce45be4e1dafab5a032e1fb1fbf97e7270a.png?width=750&amp;format=webply&amp;optimize=medium">
-        <source media="(min-width: 400px)" srcset="/media_1b4e25ce45be4e1dafab5a032e1fb1fbf97e7270a.png?width=2000&amp;format=png&amp;optimize=medium">
-        <img loading="lazy" alt="" src="/media_1b4e25ce45be4e1dafab5a032e1fb1fbf97e7270a.png?width=750&amp;format=png&amp;optimize=medium">
-      </picture>
-      `;
-    const txt = document.createElement('div');
-    txt.textContent = 'How a first-time personal loan application works';
-    carouselCard.push(pic);
-    carouselCard.push(txt);
-    carouselCards.push(carouselCard);
+const carouselCards = [];
+
+function formatDate(date) {
+    const d = new Date(Math.round((+date - (1 + 25567 + 1)) * 86400 * 1000));
+    d.setMonth(d.getMonth());
+    const monthName = d.toLocaleString('default', { month: 'short' });
+    const dateNew = `${d.getDate()} ${monthName}  ${d.getFullYear()}`;
+    return dateNew;
   }
-  return carouselCards;
+
+async function fetchData() {
+    // Creating Our XMLHttpRequest object 
+    var xhr = new XMLHttpRequest();
+    // Making our connection  
+    var url = '/query-index.json';
+    xhr.open("GET", url, false);
+    // function execute after request is successful 
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(this.responseText);
+            var dataArr = response.data;
+            for (const itr of dataArr) {
+                const carouselCard = [];
+                const pic = document.createElement('picture');
+                pic.innerHTML = `
+                    <picture>
+                        <source media="(min-width: 400px)" type="image/webp" srcset="${itr.image}">
+                        <source type="image/webp" srcset="${itr.image}">
+                        <source media="(min-width: 400px)" srcset="${itr.image}">
+                        <img loading="lazy" alt="" src="${itr.image}">
+                    </picture>
+                    `;
+                    const title = document.createElement('h2');
+                    const desc = document.createElement('p');
+                    const description = document.createElement('strong');
+                    const author = document.createElement('h5');
+                    const pdate = document.createElement("p");
+                    const publishDate = document.createElement('em');
+                    
+                    const readTime = document.createElement('h6');
+                    const readLink = document.createElement('h4');
+
+                    title.textContent = itr.title;
+                    description.textContent = itr.description;
+                    author.textContent = "By " + itr.author;
+                    publishDate.textContent = 'Published ' + formatDate(itr.date) + ' in Loans';
+                    readTime.textContent = itr.readTime;
+                    readLink.textContent = "Read > "
+                    carouselCard.push(pic);
+        
+                    var read = document.createElement("div");
+                    var carouselText = document.createElement("div");
+                    carouselText.appendChild(author);
+                    pdate.appendChild(publishDate);
+                    carouselText.appendChild(pdate);
+                    carouselText.appendChild(title);
+                    desc.appendChild(description);
+                    carouselText.appendChild(desc);
+                    
+                    read.appendChild(readLink);
+                    read.appendChild(readTime);
+                    carouselText.appendChild(read);
+
+                    carouselCard.push(carouselText);
+                    carouselCards.push(carouselCard);
+            }
+        }
+    }
+    // Sending our request 
+    xhr.send();
 }
 
 export default async function decorate(block) {
   block.innerHTML = '';
-  const carouselCards = buildCarouselCards();
+  fetchData();
   const multiImageCarousel = buildBlock('carousel', carouselCards);
   multiImageCarousel.classList.add('multiImageCarousel');
   block.appendChild(multiImageCarousel);
   decorateBlock(multiImageCarousel);
   await loadBlock(multiImageCarousel);
+  console.log("Returning from decorate");
   return multiImageCarousel;
 }
