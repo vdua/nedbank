2effe84791d907486c7ead2d9129e79b301d8003
Satya Deep Maheshwari <satyam@adobe.com>
2022-11-08 11:18:47 +0530

    chore: Accessibility fixes

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,109 +1,112 @@
 function calculateScrollbarWidth() {
-    document.documentElement.style.setProperty(
-      '--scrollbar-width',
-      `${window.innerWidth - document.documentElement.clientWidth}px`
-    );
+  document.documentElement.style.setProperty(
+    '--scrollbar-width',
+    `${window.innerWidth - document.documentElement.clientWidth}px`
+  );
+}
+
+function selectButton(block, button, row, buttons) {
+  const index = [...buttons].indexOf(button);
+  const arrows = block.parentNode.querySelector('.carousel-controls');
+
+  block.scrollTo({ top: 0, left: row.offsetLeft - row.parentNode.offsetLeft, behavior: 'smooth' });
+  buttons.forEach((r) => r.classList.remove('selected'));
+  button.classList.add('selected');
+
+  // enable arrows
+  [...arrows.children].forEach((arrow) => arrow.classList.remove('disabled'));
+
+  // disable arrows
+  if (index === 0) {
+    arrows.querySelector('.prev').classList.add('disabled');
+  } else if (index >= buttons.length - 1) {
+    arrows.querySelector('.next').classList.add('disabled');
   }
-  
-  function selectButton(block, button, row, buttons) {
-    const index = [...buttons].indexOf(button);
-    const arrows = block.parentNode.querySelector('.carousel-controls');
-  
-    block.scrollTo({ top: 0, left: row.offsetLeft - row.parentNode.offsetLeft, behavior: 'smooth' });
-    buttons.forEach((r) => r.classList.remove('selected'));
-    button.classList.add('selected');
-  
-    // enable arrows
-    [...arrows.children].forEach((arrow) => arrow.classList.remove('disabled'));
-  
-    // disable arrows
-    if (index === 0) {
-      arrows.querySelector('.prev').classList.add('disabled');
-    } else if (index >= buttons.length - 1) {
-      arrows.querySelector('.next').classList.add('disabled');
+}
+
+function getVisibleSlide(event) {
+  const { target } = event;
+  const buttons = target.nextElementSibling.querySelectorAll('button');
+  const slides = target.querySelectorAll(':scope > div');
+  const leftPosition = target.scrollLeft;
+  let leftPadding = 0;
+
+  slides.forEach((slide, key) => {
+    const offset = slide.offsetLeft;
+
+    // set first offset (extra padding?)
+    if (key === 0) leftPadding = offset;
+
+    if (offset - leftPadding === leftPosition) {
+      // trigger default functionality
+      selectButton(target, buttons[key], slide, buttons);
     }
-  }
-  
-  function getVisibleSlide(event) {
-    const { target } = event;
-    const buttons = target.nextElementSibling.querySelectorAll('button');
-    const slides = target.querySelectorAll(':scope > div');
-    const leftPosition = target.scrollLeft;
-    let leftPadding = 0;
-  
-    slides.forEach((slide, key) => {
-      const offset = slide.offsetLeft;
-  
-      // set first offset (extra padding?)
-      if (key === 0) leftPadding = offset;
-  
-      if (offset - leftPadding === leftPosition) {
-        // trigger default functionality
-        selectButton(target, buttons[key], slide, buttons);
+  });
+}
+
+export default function decorate(block) {
+  const buttons = document.createElement('div');
+  const autoPlayList = [];
+  let carouselInterval = null;
+
+  // dots
+  buttons.className = 'carousel-buttons';
+  [...block.children].forEach((row, i) => {
+    // set classes
+    [...row.children].forEach((child) => {
+      if (child.querySelector('picture')) {
+        child.classList.add('carousel-image');
+      } else {
+        child.classList.add('carousel-text');
       }
     });
-  }
-  
-  export default function decorate(block) {
-    const buttons = document.createElement('div');
-    const autoPlayList = [];
-    let carouselInterval = null;
-  
-    // dots
-    buttons.className = 'carousel-buttons';
-    [...block.children].forEach((row, i) => {
-      // set classes
-      [...row.children].forEach((child) => {
-        if (child.querySelector('picture')) {
-          child.classList.add('carousel-image');
-        } else {
-          child.classList.add('carousel-text');
-        }
-      });
-  
-      // move icon to parent's parent
-      const icon = row.querySelector('span.icon');
-      if (icon) icon.parentNode.parentNode.prepend(icon);
-  
-      /* buttons */
-      const button = document.createElement('button');
-      if (!i) button.classList.add('selected');
-      button.addEventListener('click', () => {
-        window.clearInterval(carouselInterval);
-        selectButton(block, button, row, [...buttons.children]);
-      });
-      buttons.append(button);
-      autoPlayList.push({ row, button });
+
+    // move icon to parent's parent
+    const icon = row.querySelector('span.icon');
+    if (icon) icon.parentNode.parentNode.prepend(icon);
+
+    /* buttons */
+    const button = document.createElement('button');
+    button.setAttribute('aria-label', `Carousel Button ${i}`)
+    if (!i) button.classList.add('selected');
+    button.addEventListener('click', () => {
+      window.clearInterval(carouselInterval);
+      selectButton(block, button, row, [...buttons.children]);
     });
-    block.parentElement.append(buttons);
-  
-    // arrows
-    const arrows = document.createElement('div');
-    const prev = document.createElement('button');
-    const next = document.createElement('button');
-  
-    arrows.classList.add('carousel-controls');
-    prev.classList.add('prev', 'disabled');
-    next.classList.add('next');
-    arrows.append(prev);
-    arrows.append(next);
-    [...arrows.children].forEach((arrow) => arrow.addEventListener('click', ({ target }) => {
-      const active = buttons.querySelector('.selected');
-      const index = [...buttons.children].indexOf(active);
-  
-      if (target.classList.contains('disabled')) return;
-  
-      if (target.classList.contains('prev')) {
-        [...buttons.children].at(index - 1).click();
-      } else if (target.classList.contains('next')) {
-        [...buttons.children].at(index + 1).click();
-      }
-    }));
-    block.parentElement.append(arrows);
-  
-    // attach scroll event
-    block.addEventListener('scroll', getVisibleSlide);
-  
-    calculateScrollbarWidth();
-    window.addEventListener('resize', calculateScrollbarWidth, false);
-  }
+    buttons.append(button);
+    autoPlayList.push({ row, button });
+  });
+  block.parentElement.append(buttons);
+
+  // arrows
+  const arrows = document.createElement('div');
+  const prev = document.createElement('button');
+  const next = document.createElement('button');
+
+  arrows.classList.add('carousel-controls');
+  prev.classList.add('prev', 'disabled');
+  prev.setAttribute('aria-label', 'Carosuel Previous');
+  next.classList.add('next');
+  next.setAttribute('aria-label', 'Carosuel Next');
+  arrows.append(prev);
+  arrows.append(next);
+  [...arrows.children].forEach((arrow) => arrow.addEventListener('click', ({ target }) => {
+    const active = buttons.querySelector('.selected');
+    const index = [...buttons.children].indexOf(active);
+
+    if (target.classList.contains('disabled')) return;
+
+    if (target.classList.contains('prev')) {
+      [...buttons.children].at(index - 1).click();
+    } else if (target.classList.contains('next')) {
+      [...buttons.children].at(index + 1).click();
+    }
+  }));
+  block.parentElement.append(arrows);
+
+  // attach scroll event
+  block.addEventListener('scroll', getVisibleSlide);
+
+  calculateScrollbarWidth();
+  window.addEventListener('resize', calculateScrollbarWidth, false);
+}
