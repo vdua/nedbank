0641616da770b439dd14a12415cf28256151c5d7
Satya Deep Maheshwari <m.satyadeep@gmail.com>
2023-02-24 16:49:09 +0530

    Add support for analytics (#72)
    Adding support for analytics.

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,3 +5,8 @@ import { sampleRUM } from './scripts.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
+(function adobeotm() {
+  const adobeotmscript = document.createElement('script');
+  adobeotmscript.setAttribute('src', 'https://assets.adobedtm.com/6422e0f550a2/017d80491d7e/launch-1e8527b948f6-development.min.js');
+  document.head.append(adobeotmscript);
+}());
