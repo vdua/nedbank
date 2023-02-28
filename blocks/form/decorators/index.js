ca166123d86b8c3a868062aa9cef0d615128fd91
vdua <vdua@adobe.com>
2023-03-01 00:01:11 +0530

    form id should be case insensitive

--- blocks/form/decorators/index.js
+++ blocks/form/decorators/index.js
@@ -1,6 +1,6 @@
 export default async function decorateForm(form) {
-  const { id } = form;
-  if (id.toLowerCase() === 'repayments-calculator') {
+  const id = form.id.toLowerCase();
+  if (id === 'repayments-calculator') {
     const decorateRepaymentsCalculator = (await import(`./${id}.js`)).default;
     decorateRepaymentsCalculator(form);
   }
