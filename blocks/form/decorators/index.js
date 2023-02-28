cac2b786cec9eebe6e20eb4612c79deefc8c0c48
vdua <vdua@adobe.com>
2023-02-28 12:53:27 +0530

    decorating custom term field as per the nedbank's requirement

--- blocks/form/decorators/index.js
+++ blocks/form/decorators/index.js
@@ -1,5 +1,30 @@
-import decorateRange from './range.js';
+import decorateRange, { createRange } from './range.js';
+import formatFns from '../formatting.js';
 import createQuestionMark from './tooltip.js';
+
+function decorateTermField(input) {
+  const values = Array(6).fill(1).map((x, i) => formatFns.year(i + 1));
+  if (input) {
+    const clonedInput = input.cloneNode();
+    clonedInput.type = 'hidden';
+    input.min = 0;
+    input.max = 6;
+    input.step = 1;
+    input.name += '-proxy';
+    const rangeDiv = createRange(input, ['6 Months'].concat(values));
+    rangeDiv.querySelector('input').addEventListener('input', (e) => {
+      if (e.target.value === '0') clonedInput.value = 0.5;
+      else clonedInput.value = e.target.value;
+      const event = new Event('input', {
+        bubbles: true,
+        cancelable: true,
+      });
+      clonedInput.dispatchEvent(event);
+    });
+    rangeDiv.append(clonedInput);
+    input.replaceWith(rangeDiv);
+  }
+}
 
 export default async function decorateCustom(form) {
   /** add custom tooltips */
@@ -10,4 +35,7 @@ export default async function decorateCustom(form) {
   form.querySelectorAll('.form-range-wrapper').forEach((block) => {
     decorateRange(block);
   });
+
+  const termField = form.querySelector('.form-term input');
+  decorateTermField(termField);
 }
